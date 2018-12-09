28c23cbac96995be0533382e1f17d677c43d5cd1
Harry S <tech.3038@gmail.com>
2018-12-09 03:06:50 -0800

    First GO Package

--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/harrysingh/go/stringutil"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+  fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
 
